b6670f8a63e689bf9d1fa992fb323fef8d04e64e
Andy Clement <aclement@pivotal.io>
2018-01-26 12:34:46 -0800

    First commit

---
+++ src/test/java/com/example/demo/ProxyBootApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.junit.Test;
+
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+public class ProxyBootApplicationTests {
+
+	@Test
+	public void testSomething() {
+		// hmm, tricky...
+	}
+
+}
